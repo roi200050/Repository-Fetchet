38de4e20a198b9f3aeaba058c0eb95b86291354c
roi200050 <roi200050@gmail.com>
2022-04-29 16:23:33 +0300

    init functions

---
+++ scoreChecker.py
@@ -0,0 +1,19 @@
+FOLDER_TO_CLONE = './project_to_check'
+
+def clone_repo(repo_url):
+    '''
+    The function clone GitHub repo using GitPython
+    param repo_url: url of repo
+    type repo_url: Text
+    '''
+    pass
+
+def calaulate_score():
+    '''
+    The function calaulate GitHub repo score using pip-check-reqs
+    returns repo's score as Number
+    '''
+    pass
+
+def get_score(repo_url):
+    pass
